70fce0de545b0d8a6710e0e791f1a05edc4d6f23
kunalarya873 <kunalarya873@gmail.com>
2024-07-18 23:49:37 +0530

    Starting a new project

---
+++ djChat/server/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="server_owner")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="server-category")
+     
+
